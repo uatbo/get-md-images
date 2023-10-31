1e659e967799e5a6e757fba5c6d87da66de981ce
uatbo <uatbo@outlook.com>
2023-10-31 20:38:13 +0800

    完成了md文件的读取和匹配图片标签

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
 
 func main() {
-	fmt.Println("hello")
-	fmt.Println("world")
+	// 指定文本文件路径
+	filePath := "/Users/uatbo/Files/大兵/笔记/增量表与全量表的区别.md"
+	// 读取文本文件
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("无法读取文件:", err)
+		os.Exit(1)
+	}
+	// 将文件内容转换为字符串
+	text := string(content)
+	// 定义正则表达式模式
+	pattern := `!\[(.*?)\]\((.*?)\)`
+	// 编译正则表达式
+	regexpObject := regexp.MustCompile(pattern)
+	// 查找匹配
+	matches := regexpObject.FindAllString(text, -1)
+
+	for i := 0; i < len(matches); i++ {
+		fmt.Println(matches[i])
+	}
+
 }
